e67dd2edad5616b72fda732507cdafa71dcee204
Bill <billwang7599@gmail.com>
2026-01-26 22:36:50 -0800

    added startup script

---
+++ startup.sh
@@ -0,0 +1,14 @@
+# good packages (MACOS)
+# install brew here 
+brew install stow 
+
+# tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+stow tmux
+
+# nvim
+stow nvim
+# run some command to ensure all dependancies for neovim packages are installed
+
+
+
