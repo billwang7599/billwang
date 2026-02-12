37aec558351241a80fa3887a810e31911c25b16a
Bill Wang Tesla <billwang7599@gmail.com>
2026-02-12 10:53:18 -0800

    updated startup

--- startup.sh
+++ startup.sh
@@ -1,16 +1,27 @@
 # good packages (MACOS)
-# install brew here 
-brew install stow 
+# install brew here
+brew install stow
+brew install git
 brew install lazygit
 
 # tmux
+brew install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 stow tmux
+# run prefix + I in tmux to install plugins
 
 # nvim
 brew install nvim
-# run some command to ensure all dependancies for neovim packages are installed
 brew install fd rg
+# treesitter & telescope-fzf-native need a C compiler
+xcode-select --install
+# node required for many LSP servers (pyright, ts_ls, html, css, json, yaml, bash)
+brew install node
+# optional language runtimes for LSP (uncomment as needed)
+# brew install go
+# brew install python
+# brew install rustup && rustup-init
+# brew install ruby
 stow nvim
 
 
