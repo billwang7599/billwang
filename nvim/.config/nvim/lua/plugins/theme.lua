2da7cc73db7a08ee3c03bb44eacc11622dfa1473
Bill Wang Tesla <billwang7599@gmail.com>
2026-02-12 10:50:12 -0800

    transparent background

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -6,5 +6,11 @@ return {
     vim.g.nord_contrast = true
     vim.g.nord_borders = false
     vim.cmd.colorscheme("nord")
+
+    -- Transparent background
+    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
   end,
 }
