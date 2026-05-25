9a563c4c9e594565399043b4d9266a62b16369f8
Bill Wang <billwang7599@gmail.com>
2026-05-25 15:37:17 -0500

    cf push

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -58,7 +58,7 @@ end, { noremap = true, silent = true, desc = "Copy full file path" })
 
 vim.keymap.set("n", "<Esc>", "<cmd>noh<cr>", { desc = "Clear search highlight" })
 
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<C-q>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 vim.diagnostic.config({
   virtual_text = true,
